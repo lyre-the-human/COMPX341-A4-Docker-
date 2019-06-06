63d774865e9606e97aff2edd235ab0a4efbe6084
Radcliffe, Meleena <meleena.radcliffe@gmail.com>
2019-06-06 14:57:30 +1200

    started on redis functionality of app

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import time
-
+import traceback
 import redis
 from flask import Flask
 import math
 
-app = Flask(app.py)
+app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
 
@@ -18,13 +18,27 @@ def get_hit_count():
                 raise exc
             retries -= 1
             time.sleep(0.5)
-
+@app.route('/isPrime/<number>')
 def isPrime(number):
-    for i in range(2,math.floor(number/2)): #check for factors
-        if (number % i) == 0:
-            print(num, " is not prime.")
-            return;
-    print(num, " is prime.")
+    try:
+        num = int(number)
+        if(num < 2):
+            return(str(num)+" is not prime.")
+
+        for i in range(2,math.floor(num/2)): #check for factors
+            if (num % i) == 0:
+                return(str(num)+" is not prime.")
+        #check if number was stored in redis previousl
+        if cache.get(str(num)) == None:
+            cache.set(str(num), str(num))
+            print(str(num) + "WAS STORED BY REDIS")
+        return(str(num)+" is prime.")
+    except Exception as e:
+        print(e)
+        traceback.print_stack(e)
+        return('Please provide an integer')
+
+       
     
 @app.route('/')
 def hello():
